36b6c6f9b5a345cba684f78c8642f19169596d93
PandeeDev <mayolchris@gmail.com>
2025-03-05 22:39:27 -0500

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     
-    public void PlayGame ()
+    public void PlayGame()
     {
         SceneManager.LoadScene("GameplayPrototypes");
     }
@@ -28,4 +28,5 @@ public class MainMenu : MonoBehaviour
         Debug.Log("Quit");
         Application.Quit();
     }
+    
 }
