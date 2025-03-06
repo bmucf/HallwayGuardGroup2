8d081a5996a58682edbc245bab211f57a11978eb
Jzap96 <Joshzappms@gmail.com>
2025-03-05 20:26:26 -0500

    music

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    [SerializeField] AudioClip DeathSFX;
+    [SerializeField] [Range(0f, 1f)] float Dead = .5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +16,14 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void PlayClip(AudioClip clip, float volume)
+    {
+        if (clip != null)
+        {
+            Vector3 cameraPos = Camera.main.transform.position;
+            AudioSource.PlayClipAtPoint(clip, cameraPos, volume);
+        }
     }
 }
